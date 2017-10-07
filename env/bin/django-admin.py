7341937a84d0ee9a402ce8d9f0ae16c662d9e21f
Ahmad <a.ebeid>
2017-10-07 22:45:22 +0200

    creationOfProject

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/ahmadebeid/AA0AD2700AD238D1/ITASURT/Project9AngularAndDjango/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
